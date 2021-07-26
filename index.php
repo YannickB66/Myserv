e6a03f14aeecc07e9969b93960c803f15daf0a68
YannickB66 <78077144+YannickB66@users.noreply.github.com>
2021-07-26 09:22:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Myser</title>
+    <meta name="description" content="Test de response.">
+    <meta name="author" content="Yannick">
+</head>
+
+<body>
+
+<h1>My serv</h1>
+
+
+
+
+
+</body>
+
 <?php
 
 
